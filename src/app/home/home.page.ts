abe6e46dd2442712bb998c371e75817482d873d0
John Daniel Pray <jdpray@gmail.com>
2018-10-06 15:03:43 -0400

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor() {
+    this.totalValue = ''
+    this.workingValue = '0';
+  }
+
+  buttonClick(key) {
+    if (key === '-' || key === '+' || key === '=') {
+
+    } else if (key === 'C') {
+      this.workingValue = '0';
+    } else {
+      if (this.workingValue.length > 3) {
+        return;
+      }
+      this.workingValue = String(parseInt(this.workingValue + String(key),10));
+    }
+    debugger;
+  }
 
 }
